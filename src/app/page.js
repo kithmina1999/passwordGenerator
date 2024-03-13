e7ff65cc6dbf6fccc12bcd6ca0d42607a5352939
kith1999 <kithgaming99@gnail.com>
2024-03-13 20:51:38 +0530

    completed with a basic generation function

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,165 @@
-import Image from "next/image";
+"use client"
+import { FaClipboard } from 'react-icons/fa'
+import { useForm } from './useForm';
+import { useState } from 'react';
+import { getRandomChar, getSpecialChar } from './utils';
+import {toast} from 'react-hot-toast'
+
 
 export default function Home() {
+
+  const [values, setValues] = useForm({
+    length: 6,
+    capital: false,
+    simple: true,
+    number: false,
+    symbol: false,
+  })
+
+  const [result,setResult] = useState('');
+
+  const fieldsArray = [
+    {
+      fields:values.capital,
+      getChar:()=> getRandomChar(65 , 90)
+    },
+    {
+      fields:values.simple,
+      getChar:()=> getRandomChar(97 , 122)
+    },
+    {
+      fields:values.number,
+      getChar:()=> getRandomChar(48 , 57)
+    },
+    {
+      fields:values.symbol,
+      getChar:()=> getSpecialChar()
+    }
+  ]
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    let generatePassword = "" ; 
+
+    const  checkedFields = fieldsArray.filter(({fields})=> fields );
+
+    for(let i=0; i < values.length; i++){
+      const index = Math.floor(Math.random()*checkedFields.length);
+      const letter = checkedFields[index]?.getChar();
+      
+      if(letter){
+        generatePassword += letter;
+        
+      }
+    }
+    if(generatePassword){
+      setResult(generatePassword)
+    }
+   
+  }
+
+  const handleClipboard= async()=>{
+    if(result){
+      await navigator.clipboard.writeText(result);
+      toast.success('Copied Successuly')      
+    }else{
+      toast.error('No Password to copy')
+    }
+  } 
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="max-w-[600px] mx-auto">
+
+        <h1 className="text-slate-800 font-bold text-center p-4 mb-8">Passowrd Generator</h1>
+        <form onSubmit={handleOnSubmit}>
+        <div className="flex justify-center items-center gap-2  bg-slate-100 shadow-md w-full h-[10vh] rounded-lg my-auto mb-10 text-center">
+          <input type="text" id="result" placeholder="Your password..." readOnly value={result}
+            className="mt-3 py-2 rounded-lg px-12" />
+          <div className='text-blue-900' onClick={handleClipboard}>
+            <FaClipboard></FaClipboard>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className=" flex justify-center">
+          <div className='flex flex-col '>
+            <div className=" mb-4 flex justify-center gap-4">
+            
+              <label
+                htmlFor="minmax-range"
+                className="block mb-2 text-sm font-medium text-gray-900 w-full ">
+                Passowrd Size
+              </label>
+              <span className="text-sm text-gray-500 dark:text-gray-400">5</span>
+              <input
+                id="minmax-range"
+                type="range"
+                min="6"
+                max="20"
+                name='length'
+                value={values.length}
+                onChange={setValues}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                className="mt-1 w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+              <span className="text-sm text-gray-500 dark:text-gray-400 ">20</span>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            </div>
+            {/* Enable Simples */}
+            <div className="flex items-center justify-center  mb-4">
+              <input 
+              id="default-checkbox" 
+              type="checkbox" 
+              name='simple'
+              checked={values.simple}
+              onChange={setValues} 
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600">{!values.simple ? "Enable Simples" : "Disable Simples"}</label>
+            </div>
+             {/* Enable Capitals */}
+             <div className="flex items-center justify-center  mb-4">
+              <input 
+              id="default-checkbox" 
+              type="checkbox" 
+              name='capital'
+              checked={values.capital}
+              onChange={setValues} 
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600">{!values.capital ? "Enable Capitals" : "Disable Capitals"}</label>
+            </div>
+            {/* Enable  numbers */}
+            <div className="flex items-center justify-center mb-4">
+              <input 
+              id="default-checkbox" 
+              type="checkbox"
+              name='number' 
+              checked={values.number}
+              onChange={setValues} 
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600">{!values.number ? "Enable Numbers" : "Disable Numbers"}</label>
+            </div>
+            {/* Enable characters */}
+            <div className="flex items-center justify-center mb-4">
+              <input 
+              id="default-checkbox" 
+              type="checkbox"
+              name='symbol' 
+              checked={values.symbol}
+              onChange={setValues} 
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600">{!values.symbol ? "Enable Symbols" : "Disable Symbols"}</label>
+            </div>
+           
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className="flex justify-center my-10">
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Generate
+              </button>
+            </div>
+            </div>
+        </div>
+        </form>
       </div>
     </main>
   );
